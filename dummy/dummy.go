9e2a3132cd392f2f14e94556c8bcf9968def9877
Jhon Baron <jhooomn@gmail.com>
2024-08-14 13:33:20 -0500

    Update dummy.go

--- dummy/dummy.go
+++ dummy/dummy.go
@@ -111,3 +111,115 @@ func dummy() {
     z = [6]int{1, 2, 3, 4, 5}
     fmt.Println("The value of z is:", z)
 }
+
+
+func dummy2() {
+    var x int
+    fmt.Println("Enter a number:")
+    fmt.Scanln(&x)
+
+    if x%2 == 0 {
+        fmt.Println("Even number!")
+    } else {
+        fmt.Println("Odd number!")
+    }
+
+    for i := 0; i < 10; i++ {
+        fmt.Println(i)
+    }
+
+    var y int
+    y = 5
+    fmt.Println("The value of y is:", y)
+
+    var z int = 10
+    fmt.Println("The value of z is:", z)
+
+    var a = 15
+    fmt.Println("The value of a is:", a)
+
+    b := 20
+    fmt.Println("The value of b is:", b)
+
+    var c int
+    fmt.Println("The value of c is:", c)
+
+    var d string
+    fmt.Println("The value of d is:", d)
+
+    var e bool
+    fmt.Println("The value of e is:", e)
+
+    var f float64
+    fmt.Println("The value of f is:", f)
+
+    var g []int
+    fmt.Println("The value of g is:", g)
+
+    var h map[string]int
+    fmt.Println("The value of h is:", h)
+
+    var i chan int
+    fmt.Println("The value of i is:", i)
+
+    var j func()
+    fmt.Println("The value of j is:", j)
+
+    var k interface{}
+    fmt.Println("The value of k is:", k)
+
+    var l *int
+    fmt.Println("The value of l is:", l)
+
+    var m error
+    fmt.Println("The value of m is:", m)
+
+    var n complex128
+    fmt.Println("The value of n is:", n)
+
+    var o uintptr
+    fmt.Println("The value of o is:", o)
+
+    var p byte
+    fmt.Println("The value of p is:", p)
+
+    var q rune
+    fmt.Println("The value of q is:", q)
+
+    var r []byte
+    fmt.Println("The value of r is:", r)
+
+    var s []rune
+    fmt.Println("The value of s is:", s)
+
+    var t [5]int
+    fmt.Println("The value of t is:", t)
+
+    var u [5]int = [5]int{1, 2, 3, 4, 5}
+    fmt.Println("The value of u is:", u)
+
+    var v [5]int
+    v[0] = 1
+    v[1] = 2
+    v[2] = 3
+    v[3] = 4
+    v[4] = 5
+    fmt.Println("The value of v is:", v)
+
+    var w [5]int
+    w = [5]int{1, 2, 3, 4, 5}
+    fmt.Println("The value of w is:", w)
+
+    var x [5]int
+    x = [5]int{1, 2, 3}
+    fmt.Println("The value of x is:", x)
+
+    var y [5]int
+    y = [5]int{1, 2, 3, 4, 5, 6}
+    fmt.Println("The value of y is:", y)
+
+    var z [5]int
+    z = [6]int{1, 2, 3, 4, 5}
+    fmt.Println("The value of z is:", z)
+}
+
